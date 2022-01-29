2632e602d481b8f2ec5040b7f46e0fab7c751f85
Tatiana <tanja6122212@list.ru>
2022-01-29 20:21:23 +0900

    Initial commit

---
+++ src/main/java/pro/sky/librarieshwork/LibrariesHworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.librarieshwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariesHworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrariesHworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/librarieshwork/LibrariesHworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.librarieshwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariesHworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
